a0d240528b841eeac88e6c2dc0bfe678ecdd08e5
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2025-02-28 12:56:07 +0800

    add examples (to be checked and updated)

---
+++ examples/pme/run.py
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+import numpy as np
+import torch
+from dp_dmff.dmff.nblist import TorchNeighborList
+from dp_dmff.dmff.pme import CoulombForceModule
+from dp_dmff.dmff.utils import calc_grads
+
+torch.set_default_device("cuda")
+torch.set_default_dtype(torch.float64)
+
+if __name__ == "__main__":
+    rcut = 6.0
+    n_atoms = 500
+    ethresh = 1e-4
+
+    l_box = 20.0
+
+    positions = np.random.rand(n_atoms, 3) * l_box
+    box = np.diag([l_box, l_box, l_box])
+    charges = np.random.uniform(-1.0, 1.0, (n_atoms))
+    charges -= charges.mean()
+
+    positions = torch.tensor(
+        positions,
+        requires_grad=True,
+    )
+    box = torch.tensor(
+        box,
+        requires_grad=False,
+    )
+    charges = torch.tensor(
+        charges,
+        requires_grad=False,
+    )
+
+    # calculate pairs
+    nblist = TorchNeighborList(cutoff=rcut)
+    pairs = nblist(positions, box)
+    ds = nblist.get_ds()
+    buffer_scales = nblist.get_buffer_scales()
+
+    module = CoulombForceModule(rcut=rcut, ethresh=ethresh, periodic=True)
+    energy = module(positions, box, pairs, ds, buffer_scales, {"charge": charges})
+    forces = -calc_grads(energy, positions)
